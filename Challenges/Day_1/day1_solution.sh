e72f3e3e24b54050f210c862284d26181a05c854
pranoti278 <121545847+pranoti278@users.noreply.github.com>
2023-08-02 00:07:13 +0530

    Update day1_solution.sh

--- Challenges/Day_1/day1_solution.sh
+++ Challenges/Day_1/day1_solution.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#echo the string 
 echo 'first challange of tws'
+
+#assigning value to variable user will provide value 
 read a
 read b
 echo c= $((a+b))
+
+#built in function
+echo "show directory path: $pwd"
+echo "$$"
+echi "to perform arithmetic operations:$let"
+
+$ cp /tmp/a/* /tmp/b/
+
+
